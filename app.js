9931a1430ef22ef89d23541f53427b26345ac150
Andrii <andrii.nikolaienko@yandex.com>
2017-08-17 19:14:59 +0300

    added few functions + post request

--- app.js
+++ app.js
@@ -1,17 +1,70 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+var option = {
+    server: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    }
+};
+
+var db = mongoose.connect('mongodb://127.0.0.1:27017/booksapi', option).then(function(){
+    //connected successfully
+    console.log('Successfully connected to DB.');
+}, function(err) {
+    //err handle
+    console.log('Error connecting to DB: ' + err);
+});
+
+var Book = require('./models/bookModel');
 var app = express();
-
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var apiRouter = express.Router();
 
 apiRouter.route('/Books')
-    .get(function(req, res){
-        var responseJson = { hello: "This is my books API."};
+    .post(function(req, res){
+        var book = new Book(req.body);
 
-        res.json(responseJson);
+        console.log('Received a book: ' + book);
+        book.save();
+        res.status(200).send(book);
+    })
+    .get(function(req, res){
+
+        var query = {};
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        })
     });
+apiRouter.route('/Books:bookId')
+    .get(function(req, res){
+    
+            Book.findById(req.params.bookId, function(err, book) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(book);
+            })
+        });
 
 app.use('/api', apiRouter);
 
